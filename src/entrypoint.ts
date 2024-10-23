6ff810e00b487c20aefcb9f45f2002aea9285e42
eisack <eisack@gmail.com>
2024-10-23 23:04:00 +1300

    dropdown as a function. Hot damn!

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -2,9 +2,11 @@ import type {Alpine} from 'alpinejs';
 import collapse from '@alpinejs/collapse';
 import intersect from '@alpinejs/intersect';
 import focus from '@alpinejs/focus';
+import anchor from '@alpinejs/anchor'
 
 export default (Alpine) => {
     Alpine.plugin(collapse);
     Alpine.plugin(intersect);
     Alpine.plugin(focus);
+    Alpine.plugin(anchor);
 };
