dc7c3b12ac113563d0288cc04537777b41e1a39a
eisack <eisack@gmail.com>
2024-10-28 00:37:22 +1300

    View transitions. Wow

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -5,8 +5,23 @@ import focus from '@alpinejs/focus';
 import anchor from '@alpinejs/anchor'
 
 export default (Alpine) => {
+    // Register plugins
     Alpine.plugin(collapse);
     Alpine.plugin(intersect);
     Alpine.plugin(focus);
     Alpine.plugin(anchor);
+
+    // Define custom data for Alpine
+    Alpine.data("astro", () => ({
+        // Track whether the page has loaded
+        pageLoaded: 1,
+        refreshOnPageLoad: {
+            // Refresh on page load event
+            ["@astro:page-load.document"]() {
+                this.pageLoaded++;
+            },
+        },
+    }));
+
+    return Alpine;
 };
